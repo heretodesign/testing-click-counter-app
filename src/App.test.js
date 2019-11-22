88333ff0f71523fd71163b8639cd95bba522de82
abel <abel@Supas-MBP.localdomain>
2019-11-22 18:52:40 +0800

    started testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
+  const bigComponent = wrapper.find("[data-test='component-big-component']");
+  expect(appComponent.length).toBe(1);
+}); 
+
+test('renders increment button', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toBeTruthy();
+});
+test('renders counter display', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toBeTruthy();
+});
+test('renders counter starts at 0', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toBeTruthy();
+});
+test('renders clicking button increments counter display', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toBeTruthy();
 });
